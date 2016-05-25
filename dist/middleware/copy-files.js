a99651940763108611de1821211ebead98125f90
Luke <luke@hemamaps.com.au>
2016-05-26 09:56:36 +1000

    BUILD

---
+++ dist/middleware/copy-files.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var vfs = require('vinyl-fs');
+var map = require('map-stream');
+
+module.exports = function (sourceFolder, targetFolder, configuration, filePathsArg) {
+    var files = filePathsArg;
+
+    if (filePathsArg.include) {
+        var files = filePathsArg.include;
+    }
+
+    if (filePathsArg.exclude) {
+        filePathsArg.exclude.forEach(function (path) {
+            files.push('!' + path);
+        });
+    }
+
+    return new Promise(function (resolve, reject) {
+        vfs.src(files, { base: sourceFolder }).pipe(vfs.dest(targetFolder)).on('finish', function () {
+            resolve();
+        });
+    });
+};
+//# sourceMappingURL=copy-files.js.map

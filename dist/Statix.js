51f75f5db7826e8ee779b84277fbe111f65fff95
Luke <luke@hemamaps.com.au>
2016-05-26 13:16:18 +1000

    BUILD

--- dist/Statix.js
+++ dist/Statix.js
@@ -13,6 +13,8 @@ var glob = require('glob');
 var fs = require('fs');
 var vfs = require('vinyl-fs');
 var gaze = require('gaze');
+var rimraf = require('rimraf');
+var ncp = require('ncp').ncp;
 
 var Statix = function () {
     function Statix(configuration) {
@@ -29,20 +31,24 @@ var Statix = function () {
             var length = this.middleware.length - 1;
             var currentRunning = 0;
 
-            var run = function () {
-                this._setUpMiddleware(this.middleware[currentRunning]).then(function () {
-                    if (currentRunning < length) {
-                        currentRunning++;
-                        run();
-                    } else {
-                        this.watchFunctions.forEach(function (func) {
-                            func();
-                        });
-                    }
-                }.bind(this));
-            }.bind(this);
-
-            return run();
+            return new Promise(function (resolve, reject) {
+                var run = function () {
+                    this._setUpMiddleware(this.middleware[currentRunning]).then(function () {
+                        if (currentRunning < length) {
+                            currentRunning++;
+                            run();
+                        } else {
+                            resolve();
+                            if (this._configuration.useFileWatch === true) {
+                                this.watchFunctions.forEach(function (func) {
+                                    func();
+                                });
+                            }
+                        }
+                    }.bind(this));
+                }.bind(this);
+                run();
+            }.bind(this));
         }
     }, {
         key: '_setUpMiddleware',
@@ -52,10 +58,7 @@ var Statix = function () {
             if (middleware.watch !== false) {
                 this.watchFunctions.push(function () {
                     gaze(directories, function (err, watcher) {
-                        console.log('watching');
-
                         watcher.on('all', function (event, filepath) {
-                            console.log('watcher triggered', filepath);
                             middleware.run(this._configuration.sourceFolder, this._configuration.tmpFolder, middleware.configuration, filepath);
                         }.bind(this));
                     }.bind(this));
@@ -116,27 +119,44 @@ var Statix = function () {
     }, {
         key: 'server',
         value: function server() {
-            this._runMiddleware();
+            this._buildTemp().then(function () {
+                console.log('Server started on localhost:' + this._configuration.port);
+                var app = connect();
+                if (this._configuration.useFileWatch === true) {
+                    app.use(connectLiveReload());
+                    this._initiateLiveReload();
+                }
 
-            var app = connect();
-            if (this._configuration.useFileWatch === true) {
-                app.use(connectLiveReload());
-                this._initiateLiveReload();
-            }
+                app.use(serveStatic(this._configuration.tmpFolder));
 
-            app.use(serveStatic(this._configuration.tmpFolder));
-
-            var server = http.createServer(app).listen(this._configuration.port);
+                var server = http.createServer(app).listen(this._configuration.port);
+            }.bind(this));
         }
     }, {
         key: '_buildTemp',
         value: function _buildTemp() {
-            console.log('build temp');
+            return this._runMiddleware();
+        }
+    }, {
+        key: '_copyTmpToBuild',
+        value: function _copyTmpToBuild() {
+            return new Promise(function (resolve, success) {
+                rimraf(this._configuration.outputFolder, function () {
+                    ncp(this._configuration.tmpFolder, this._configuration.outputFolder, function (err) {
+                        if (err) {
+                            console.error(err);
+                        }
+                        resolve();
+                    }.bind(this));
+                }.bind(this));
+            }.bind(this));
         }
     }, {
         key: 'build',
         value: function build() {
-            console.log('building');
+            this._buildTemp().then(function () {
+                this._copyTmpToBuild();
+            }.bind(this));
         }
     }]);
 
--- dist/demo-build.js
+++ dist/demo-build.js
@@ -3,7 +3,7 @@
 var Statix = require('./Statix');
 var demoConfiguration = require('./demo-configuration');
 
+demoConfiguration.useFileWatch = false;
 var statix = new Statix(demoConfiguration);
-
-statix.server();
-//# sourceMappingURL=demo.js.map
+statix.build();
+//# sourceMappingURL=demo-build.js.map
